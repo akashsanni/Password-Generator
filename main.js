c16502bf19ec12c907fdc0a5faa48697de302018
Akash kumar <akash8825317696@gmail.com>
2023-01-19 20:53:54 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+const passwordBox = document.querySelector('.password-box')
+const firstPassword = document.querySelector('.first-password')
+const secondPassword = document.querySelector('.second-password')
+const generatePasswordBtn = document.querySelector('#generate-password-btn')
+generatePasswordBtn.addEventListener('click',function(e){
+    e.target.innerText = 'Generate Again!'
+    passwordBox.style.visibility = 'visible'
+    firstPassword.textContent=""
+    secondPassword.textContent=""
+    for(let i=0 ; i < 15 ; i++){
+        let password1 = Math.floor(Math.random()*characters.length)
+        firstPassword.textContent += characters[password1];
+    }
+    
+    for(let i=0 ; i < 15 ; i++){
+        let password2 = Math.floor(Math.random()*characters.length);
+        secondPassword.textContent += characters[password2];
+    }
+
+})
