ad7b4430d4fe5806afdcfb2d7684251785203704
Akash kumar <akash8825317696@gmail.com>
2023-01-21 22:18:01 +0530

    commit 2
    alpha-numeric password

--- main.js
+++ main.js
@@ -21,3 +21,24 @@ generatePasswordBtn.addEventListener('click',function(e){
     }
 
 })
+
+//for alpha-numeric-password
+const alphaNumeric = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const alphaNumericPassword = document.querySelector('#alpha-numeric-password')
+
+alphaNumericPassword.addEventListener('click',function(e){
+    e.target.innerText = 'Generate Again!'
+    passwordBox.style.visibility = 'visible'
+    firstPassword.textContent=""
+    secondPassword.textContent=""
+    for(let i=0 ; i < 13 ; i++){
+        let password1 = Math.floor(Math.random()*alphaNumeric.length)
+        firstPassword.textContent += alphaNumeric[password1];
+    }
+    
+    for(let i=0 ; i < 13 ; i++){
+        let password2 = Math.floor(Math.random()*alphaNumeric.length);
+        secondPassword.textContent += alphaNumeric[password2];
+    }
+
+})
